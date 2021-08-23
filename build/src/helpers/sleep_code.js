6103fb779787a3a4753a9efc486ea34eb9211af3
Alexander Petrovskiy <apetrovskiy2@hotmail.com>
2021-08-23 16:07:59 +0300

    updates

--- build/src/helpers/sleep_code.js
+++ build/src/helpers/sleep_code.js
@@ -1,9 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-var sleep = function (seconds) {
-    return new Promise(function (resolve) {
+export default (seconds) => {
+    return new Promise((resolve) => {
         setTimeout(resolve, seconds * 1000);
     });
 };
-exports.sleep = sleep;
